da5c5febf82d74158ea42fbd7bfc85f7c3a848f8
Chris Roberts <croberts@hashicorp.com>
2025-02-13 18:04:48 -0800

    Clear cache between runs

--- styrene.sh
+++ styrene.sh
@@ -10,14 +10,14 @@ export PYTHONPATH=.
 
 if test "x$MSYSTEM" = "xMSYS"; then
     echo >&2 "+++ MSYS shell detected, building all native architectures."
-    # Scrub the packages in the cache prior to run
-    echo >&2 "--- removing existing packages in cache"
-    rm -f /var/cache/pacman/pkg/*.zst
-    contents="$(ls -l /var/cache/pacman/pkg/)"
-    printf >&2 "*** contents of cache directory:\n%s\n" "${contents}"
-
     for s in MINGW64 MINGW32; do
         echo >&2 "+++ Running $PACKAGE in a $s login shell..."
+        # Scrub the packages in the cache prior to run
+        echo >&2 "--- removing existing packages in cache"
+        rm -f /var/cache/pacman/pkg/*
+        contents="$(ls -l /var/cache/pacman/pkg/)"
+        printf >&2 "*** contents of cache directory:\n%s\n" "${contents}"
+
         cmd="$PYTHON $PACKAGE"' "$@"'
         MSYSTEM=$s \
         CHERE_INVOKING=1 \
